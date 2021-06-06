41a39c9dc18da3c2fefcc25df809f7686d29e48c
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-07 00:06:55 +0300

    Create Team Object and its properties

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -8,4 +8,11 @@ public class Team {
     private  String cause;
     private int max_size;
     private static List<Team> instances=new ArrayList<Team>();
+
+    public Team(String name, String cause, int max_size){
+        this.name=name;
+        this.cause=cause;
+        this.max_size=max_size;
+    }
+
 }
