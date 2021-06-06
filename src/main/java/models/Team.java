3e146c347bfcb2ff87e3f6480393217876377ab2
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-07 00:01:46 +0300

    Add properties of the class Team and made them private

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,4 +1,11 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+    private String name;
+    private  String cause;
+    private int max_size;
+    private static List<Team> instances=new ArrayList<Team>();
 }
